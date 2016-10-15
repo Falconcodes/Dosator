5bfbc001a78950baacc001924a7e75f8f0b4fc5c
Denis <sclad007@mail.ru>
2016-10-15 03:17:46 +0300

    New

--- dosator.c
+++ dosator.c
@@ -7,15 +7,20 @@
 #include <mega328p.h>
 #include <delay.h>
 
-#define SERVO_MAX 0xA0
-#define SERVO_MIN 0x20
+#define SERVO_MAX 0xB0
+#define SERVO_MIN 0x22
+
+//кнопки
+#define UP    !PINC.4
+#define DOWN  !PIND.7
+#define ENTER !PINC.5
 
 //аноды
 #define A4 PORTB.0
 #define A2 PORTB.4
 #define A3 PORTB.3
-#define A1 PORTC.2=PORTC.3
-#define A0 PORTC.4=PORTC.5
+#define A1 PORTC.2
+#define A0 PORTC.3
 
 //сегменты
 #define a PORTC.1 //право верх
@@ -32,22 +37,26 @@ signed char min, sec;
 
 void data_conv(unsigned char, unsigned char);
 void dig_send(unsigned char);
+void putchar(unsigned char, unsigned char);
 
 // Timer 0 overflow interrupt service routine
 interrupt [TIM0_OVF] void timer0_ovf_isr(void)
 {
   TCNT0=0xE8;
   shift++;
-  if (shift==5) shift=1;
+  if (shift==5) shift=0;
   time_count++;
   if (time_count>650) 
     { 
     time_count=0;
     sec--;
     } 
-  A1=A2=A3=A4=0;
+  A0=A1=A2=A3=A4=0;
   switch (shift) 
     {
+    case 0: A0=1;
+    dig_send(0);
+           break;
     case 1: A1=1;
     dig_send(1);
            break;
@@ -68,22 +77,34 @@ interrupt [TIM1_COMPA] void timer1_compa_isr(void)
 {
 }
 
-unsigned int data, i, step_time, last_time;
-unsigned char ocr=0x50, total_vol;
-unsigned char dig[5];
+float step_time;
+unsigned int data, i, last_time;
+eeprom unsigned char ocr=0xB0;
+unsigned char total_vol, dose_steps=1;
+unsigned char dig[5]; //по количеству разрядов (анодов) - буфер для хранения кода символов в массиве numbers[][] - символов, которые поместим потом на дисплей на соответствующие аноды 0..4
 
-unsigned char numbers[10][2]=
+//первый столбец - маска для порта С, второй - для порта D. Старший бит для порта D - не для индикатора, это pull-up кнопки на PD7
+unsigned char numbers[][2]=
 {
-0b00, 0b10000, //0
-0b10, 0b10111, //1
-0b00, 0b01001, //2
-0b00, 0b00011, //3
-0b10, 0b00110, //4 
-0b01, 0b00010, //5
-0b01, 0b00000, //6
-0b00, 0b10111, //7
-0b00, 0b00000, //8
-0b00, 0b00010  //9
+0b00, 0b110000, //0
+0b10, 0b110111, //1
+0b00, 0b101001, //2
+0b00, 0b100011, //3
+0b10, 0b100110, //4 
+0b01, 0b100010, //5
+0b01, 0b100000, //6
+0b00, 0b110111, //7
+0b00, 0b100000, //8
+0b00, 0b100010, //9
+0b10, 0b110111, //: (10) для анода A0
+0b11, 0b101111, //- (11) для А1-А4
+0b11, 0b101000, //t (12)
+0b10, 0b110011, //_№1 и : для А0 (13)
+0b00, 0b110100, //_№2,3,4 и : для А0 (14)
+0b10, 0b110110, //_№3 и : для А0 (15)
+0b00, 0b110111, //_№4 и : для А0 (16)
+0b00, 0b100000, //все "_" и : для А0 (17)
+0b11, 0b111111  //NULL (18) - ни один индикатор разряда не светится
 };
       
 void main(void)
@@ -101,54 +122,152 @@ OCR1AL=ocr; //приравниваем к значению, которое было перед отключением
 // Timer/Counter 1 Interrupt(s) initialization
 TIMSK1=(1<<OCIE1A);
 
-DDRD=255;
-DDRB=255;
-DDRC.0=DDRC.1=DDRC.2=DDRC.3=DDRC.4=DDRC.5=1;
-PORTB=0;
-PORTC.0=PORTC.1=1;
-PORTD=255;
-OCR1AL=SERVO_MAX;
-delay_ms(500);
-OCR1AL=SERVO_MAX-20;
+DDRB=0b00111111;
+DDRC=0b00001111;
+DDRD=0b01111111;
 
-min=2;
+PORTC=0b11110000;
+PORTD=255;
+
+min=5;
 sec=0;
+
+data_conv(18, 0); //разделительные точки и первое подчеркивание
+putchar(11, 1); //и прочерки "-" на всех разрядах
+putchar(11, 2);
+putchar(11, 3);
+putchar(11, 4);
 
 // Global enable interrupts
 #asm("sei");
 
-//for (OCR1AL=0xB0; OCR1AL>0x20; OCR1AL--) 
-//{
-//  delay_ms(25);
-//}
+//при включении спускаем плавно с последнего угла до нуля
+for ( ; ocr>0x20; ocr--) 
+  {
+  OCR1AL=ocr;
+  delay_ms(20);
+  }
 
-total_vol=SERVO_MAX-SERVO_MIN; //общее количество шагов сервопривода
+data_conv(15, 0); //разделительные точки и второе подчеркивание
+
+//установка времени, на протяжении которого дозируем
+while(!ENTER)
+ {if (UP)              //при коротком нажатии инкремент на 1
+  {if (min<99) min++;  
+   delay_ms(250);
+   while(UP)           //при удержании - ускоренный набор
+    {data_conv(min, 2);
+    delay_ms(100);
+    if (min<99) min++;
+    }
+  } 
+  
+  if (DOWN)
+  {if (min>1)  min--; 
+   delay_ms(250);
+   while(DOWN)
+    {data_conv(min, 2);
+    delay_ms(100);
+    if (min>1)  min--;
+    }
+  }
+ data_conv(min, 2);
+ }
+ 
+ data_conv(17, 0); //разделительные точки и все подчеркивания
+ delay_ms(1000);
+ data_conv(14, 0); //разделительные точки и третье подчеркивание 
+ delay_ms(1);
+ 
+ while(ENTER); //ждем пока отпустят кнопку
+ 
+ delay_ms(1);
+ 
+ //установка объема
+ while(!ENTER)
+ {if (UP)              //при коротком нажатии инкремент на 1
+  {if (ocr<SERVO_MAX) OCR1AL=++ocr;  
+   delay_ms(250);
+   while(UP)           //при удержании - ускоренный набор
+    {delay_ms(25);
+    if (ocr<SERVO_MAX) OCR1AL=++ocr;
+    }
+  } 
+  
+  if (DOWN)
+  {if (ocr>SERVO_MIN)  OCR1AL=--ocr; 
+   delay_ms(250);
+   while(DOWN)
+    {delay_ms(25);
+    if (ocr>SERVO_MIN) OCR1AL=--ocr;
+    }
+  }
+
+ }
+ 
+ for (i=0; i<7; i++)
+   {
+   data_conv(17, 0); //разделительные точки и все подчеркивания
+   delay_ms(150);
+   data_conv(18, 0); //разделительные точки и все подчеркивания
+   delay_ms(50);
+   }
+ 
+ while(ENTER); //ждем пока отпустят кнопку
+ 
+ delay_ms(1);
+
+data_conv(17, 0); //разделительные точки и все подчеркивания
+delay_ms(500);
+
+data_conv(sec, 1);
+data_conv(10, 0); //разделительные точки и нет подчеркиваний
+
+if (ocr>(SERVO_MAX-30)) OCR1AL=ocr=SERVO_MAX-30; //сразу подводим сервопривод к началу аутивной зоны шприца
+
+total_vol=ocr-SERVO_MIN; //общее количество шагов сервопривода
 last_time=min*60; //время предыдущего шага - первое значение
 step_time=(float)last_time/(float)total_vol; //время между шагами в миллисекундах
 
-
+sec=0;
 while (1)
   {
    if (sec<0) 
      {
      sec=59; 
      min--;
+     if (min<0) 
+       {
+       sec=0; 
+       min=0;
+       while(1); //закончили добавлять - больше ничего не делаем
+       }
      }
    if ((last_time - min*60 - sec) > step_time)
      {
-     if (OCR1AL>SERVO_MIN) OCR1AL--;
+     if (ocr>SERVO_MIN) OCR1AL=ocr=ocr-1-(int)((ocr-SERVO_MIN)/(min*60+sec));
      last_time=min*60+sec;  
      }
-   data_conv(min, 1);
-   data_conv(sec, 0);
-   
+   data_conv(min, 2);
+   data_conv(sec, 1);
+   if (sec%2==0) data_conv(10, 0); //эта и следующая строка - мигание разделительных точек
+   else data_conv(18, 0);
   }
 } /////// КОНЕЦ ГЛАВНОГО ЦИКЛА ///////
 
 void data_conv(unsigned char value, unsigned char group)
+{ if (group!=0)
+  {
+  dig[group*2-1] = value%10;
+  dig[group*2]   = value/10;
+  }
+  else 
+  dig[0] = value;
+}
+
+void putchar(unsigned char value, unsigned char place)
 {
-  dig[1+group*2] = value%10;
-  dig[2+group*2] = value/10;
+ dig[place] = value;
 }
 
 void dig_send(unsigned char place)
